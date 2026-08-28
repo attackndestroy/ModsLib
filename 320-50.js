7c02a04e4f52c5badb0ba48ee2ec9aff9969018f
ATT/DES <attackndestroy@proton.me>
2026-08-28 21:44:23 +0100

    Create 320-50.js

---
+++ 320-50.js
@@ -0,0 +1,9 @@
+var atOptions = {
+    'key': '28d1f1f838ea883f1f7fc0d4733ebba6',
+    'format': 'iframe',
+    'height': 50,
+    'width': 320,
+    'params': {}
+};
+
+document.write('<scr' + 'ipt type="text/javascript" src="https://www.highrevenueformat.com/28d1f1f838ea883f1f7fc0d4733ebba6/invoke.js"></scr' + 'ipt>');
